9e034885cd85a9659ea20524ce656a0955a96d96
Jizag12 <joseizaguirre902@gmail.com>
2025-08-12 09:39:30 -0400

    this is my first commit on this project lets see how it goes

---
+++ developer-capstone/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the Developer Capstone Project</h1>
+        <p>This is a simple React application.</p>
+      </header>
+      <main>
+        <p>Explore the features and functionalities of this app!</p>
+      </main>
+      <footer className="App-footer">
+        <p>&copy; 2025 Developer Capstone Project</p>
+      </footer>
+
+    </div>
+  );
+}
+
+export default App;
